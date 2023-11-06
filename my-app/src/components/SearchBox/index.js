1e30c0b8a822bb8c3f9a04cd009f1af15f56f9cb
Prathmesh Gawade <gawadepr101@gmail.com>
2023-11-06 14:49:58 +0530

    trial complete

--- my-app/src/components/SearchBox/index.js
+++ my-app/src/components/SearchBox/index.js
@@ -10,7 +10,7 @@ export default () => {
       }}
     >
       <input
-        placeholder="trial"
+        placeholder="Search an album of your choice"
         style={{ flex: 1, borderRadius: "8px 0px 0px 8px", padding: "8px" }}
       />
       <button
